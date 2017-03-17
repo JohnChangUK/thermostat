5ea1188b1ce653a00e9d37009202c4ac4c122404
John Chang <johnchang9094@gmail.com>
2017-03-17 14:37:42 +0000

    Implementing API from openweathermap

--- src/Interface.js
+++ src/Interface.js
@@ -1,11 +1,13 @@
+  
 
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $.get('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=e4f4eb126438f8cf7d9803f8ed0e18db&units=metric', function(data) {
-    $('#current-temperature').text(data.main.temp);
-  });
+  function updateTemperature() {
+  $('#temperature').text(thermostat.currentTemperature);
+  $('#temperature').attr('class', thermostat.checkEnergyUsage());
+  }
 
   $('#temperature-up').click(function() {
     thermostat.up();
@@ -34,15 +36,26 @@ $(document).ready(function() {
       updateTemperature();
   });
 
+  function displayWeather(city) {
+ var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+ var token = '&appid=e4f4eb126438f8cf7d9803f8ed0e18db';
+ var units = '&units=metric';
+ $.get(url + token + units, function(data) {
+   $('#current-temperature').text(data.main.temperature);
+ });
+
+
+ displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
+
   $('#current-city').change(function() {
     var city = $('#current-city').val();
     $.get('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=e4f4eb126438f8cf7d9803f8ed0e18db&units=metric', function(data) {
-      $('#current-temperature').text(data.main.temp);
+      $('#current-temperature').text(data.main.temperature);
     });
-  });
-
-  function updateTemperature() {
-  $('#temperature').text(thermostat.currentTemperature);
-  $('#temperature').attr('class', thermostat.checkEnergyUsage());
-}
-});
+  
